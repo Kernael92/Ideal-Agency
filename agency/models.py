5d2abd8aaf28e70793aaaa3dd46b5084780ca870
Kernael92 <kernaelapuko5@gmail.com>
2019-01-16 13:16:41 +0300

    create user group class and a model class

--- agency/models.py
+++ agency/models.py
@@ -2,5 +2,39 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    is_model = models.BooleanField(default=False)
+    is_client = models.BooleanField(default=False)
+
+
+class Model(models.Model):
+    LOCATION_CHOICE = (
+        ('Mombasa','Mombasa'),
+        ('Nairobi','Nairobi'),
+        ('Kisumu','Kisumu'),
+        ('Nakuru','Nakuru'),
+    )
+
+    GENDER_CHOICE = (
+        ('F','FEMALE'),
+        ('M','MALE'),
+    )
+
+    HEIGHT_CHOICE = (
+        (190,'190cm'),
+        (185,'185cm'),
+        (180,'180cm'),
+        (175,'175cm'),
+        (170,'170cm'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
+    phone_number = models.CharField(max_length = 20)
+    height = models.IntegerField(choices=HEIGHT_CHOICE, default = 0)
+    gender = models.CharField(choices = GENDER_CHOICE,max_length = 7)
+    location = models.CharField(choices=LOCATION_CHOICE,max_length = 245)
+
+   
+
